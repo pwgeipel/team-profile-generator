ea09db361a8f388f578af0bf9f54b30c72bb87a8
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-11-06 14:32:33 -0600

    index consts

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateHTML = require("./src/generateHTML.js");
+
+const Manager = require("./lib/Manager.js")
+const Engineer = require("./lib/Engineer.js")
+const Intern = require("./lib/Intern.js")
+
+
