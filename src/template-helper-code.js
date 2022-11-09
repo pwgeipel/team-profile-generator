caf3522b1cf69845fff28c2f20436f1da796a935
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-11-09 15:23:26 -0600

    template-helper-code

---
+++ src/template-helper-code.js
@@ -0,0 +1,19 @@
+const generateTeam = (team) => {
+    const createManager = (manager) => {
+        return `
+        <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Manager name</h3>
+                        <h3 class="card-title">manager</h3>
+                    </div>
+                    <div>
+                        <ul class="list-group">
+                            <li class="list-group-item">ID: 3</li>
+                            <li class="list-group-item">Email: <a href="#"></a>emailGoesHere</li>
+                            <li class="list-group-item">Office: 3</li>
+                        </ul>
+                    </div>
+                </div>
+        `
+    }
+}
