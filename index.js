6b6e09f3627312dd14c553879ed34a28e4170a17
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-11-08 21:00:34 -0600

    manager.js

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const generateHTML = require("./src/generateHTML.js");
+import inquirer from 'inquirer';
+import fs from "fs"
+// import generateHTML from './src/generateHTML.js'
 
-const Manager = require("./lib/Manager.js")
-const Engineer = require("./lib/Engineer.js")
-const Intern = require("./lib/Intern.js")
+// const Manager = require("./lib/Manager.js")
+// const Engineer = require("./lib/Engineer.js")
+// const Intern = require("./lib/Intern.js")
 
+// const questions = 
+const employeeGroup = [];
 
+const userInput = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?",
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?",
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?",
+        },
+    ])
+    .then (function(input) {
+        console.log(input)
+        let testEngineer = new Engineer(
+            input.name,
+            input.id,
+            input.email,
+            input.github,
+        )
+        employeeGroup.push(testEngineer)
+        // addEmployee()
+    })
+
+}
+function makeProfile () {
+    const template = generateHTML(employeeGroup)
+}
+userInput();
